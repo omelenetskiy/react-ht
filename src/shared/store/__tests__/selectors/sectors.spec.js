5ef22427e92b177d71c5a3250f4ff0208c99a63a
omelenetskiy <amelianetski@gmail.com>
2019-03-30 10:53:02 +0300

    update selectors tests

--- src/shared/store/__tests__/selectors/sectors.spec.js
+++ src/shared/store/__tests__/selectors/sectors.spec.js
@@ -6,29 +6,15 @@ import {
 } from "../../selectors";
 
 const state = {
-  movies: [],
-  movie: {}
+  movies: "movies",
+  movie: "movie"
 };
-
-const mockGetMovies = jest.fn();
-const mockGetMovie = jest.fn();
 
 jest.mock("reselect", () => ({ createSelector: () => "selector" }));
 
-jest.mock("../../selectors", () => ({
-  getMovies: mockGetMovies,
-  getMovie: mockGetMovie,
-  getMoviesState: jest.fn(),
-  getMovieState: jest.fn()
-}));
-
 describe("Selectors", () => {
   it("Should create selector", () => {
-    getMovies();
-    getMovie();
-    getMoviesState();
-    expect(getMovies).toBeCalled();
-    expect(getMovie).toBeCalled();
-    expect(getMoviesState).toBeCalled();
+    expect(getMovies(state)).toBe("movies");
+    expect(getMovie(state)).toBe("movie");
   });
 });
